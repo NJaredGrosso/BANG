510deb455bdf854475809b6767e2545bbac7def5
Jared Grosso <jaredgrosso2001@gmail.com>
2022-10-16 13:03:48 -0300

    Cambios esteticos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import NavBar from "./components/NavBar/NavBar";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-
+import "./App.css"
 
 function App() {
 
