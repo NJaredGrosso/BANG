2a6eae6f9867ba29605ebec56a983bce3fb1783b
Jared Grosso <jaredgrosso2001@gmail.com>
2022-11-01 17:23:35 -0300

    Cambios esteticos

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom"
 function Logo() {
   return (
     <div className="logo">
-        <Link to="/"><img src="https://seeklogo.com/images/B/bang-comic-book-exclamation-logo-CF14DC98BE-seeklogo.com.png" /></Link>
+        <Link to="/"><img src={require("../../assets/logo.png")} /></Link>
     </div>
   )
 }
 
 export default Logo
+
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,11 +9,11 @@ function NavBar() {
         <Logo/>
           <nav className="navBar">
               <ul className="navBarList">
-                  <Link to="/" className="navBarLink"><li>Inicio</li></Link>
-                  <Link to="/category/marvel" className="navBarLink"><li>Marvel</li></Link>
+                  <Link to="/" className="navBarLink"><li>INICIO</li></Link>
+                  <Link to="/category/marvel" className="navBarLink"><li>MARVEL</li></Link>
                   <Link to="/category/dc" className="navBarLink"><li>DC</li></Link>
-                  <Link to="/category/ciencia-ficcion" className="navBarLink"><li>Ciencia Ficcion</li></Link>
-                  <Link to="/category/manga" className="navBarLink"><li>Mangas</li></Link>
+                  <Link to="/category/ciencia-ficcion" className="navBarLink"><li>CIENCIA FICCION</li></Link>
+                  <Link to="/category/manga" className="navBarLink"><li>MANGAS</li></Link>
               </ul>
           </nav>
         <CartWidget/>
