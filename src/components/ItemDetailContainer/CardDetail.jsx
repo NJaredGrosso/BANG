7d778c15281ba34c58df3cd61c4d09228a9c4be0
Jared Grosso <jaredgrosso2001@gmail.com>
2022-11-01 17:23:17 -0300

    agregamos las funciones de carrito

--- src/components/ItemDetailContainer/CardDetail.jsx
+++ src/components/ItemDetailContainer/CardDetail.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import ItemCount from "./ItemCount";
 import "./cardDetail.css"
 
 function CardDetail({comic}) {
+    const [cuenta, setCuenta] = useState(0)
+
+    function handleAddToCart(cuenta) {
+        setCuenta(cuenta)
+        console.log("Se agrego al carrito", comic, cuenta)
+    }
+
+
+
     return (
         <div id="cardDetail">
             <div id="itemDetailPortada"><img id="itemDetailPortadaImg" src={comic.portada} alt="Portada del comic" /></div>
                 <div id="infoItem">
                     <h1>{comic.title}</h1>
                     <h3>{comic.precio}</h3>
-                    <ItemCount stock={comic.stock}/>
-                    <button id="infoItemButton">Añadir al carrito</button>
+                    {cuenta === 0 ? (
+                        <ItemCount 
+                            stock={comic.stock}
+                            onAddToCart={handleAddToCart}/>
+                    ) : (
+                        <a href="/cart">Mostrar Carrito</a>
+                    )}
+                    
+                    
                     <p>{comic.descripcion}</p>
                 </div>
         </div>
--- src/components/ItemDetailContainer/ItemCount.jsx
+++ src/components/ItemDetailContainer/ItemCount.jsx
@@ -1,12 +1,12 @@
 import {useState} from 'react'
 import "./itemCount.css"
 
-function ItemCount(props) {
+function ItemCount({stock, onAddToCart }) {
 
     const [cantidad, setCantidad] = useState(1)
 
     function add() {
-        if (cantidad < props.stock) {
+        if (cantidad < stock) {
             console.log("suma 1")
             setCantidad(cantidad + 1);
         }else{
@@ -24,11 +24,19 @@ function ItemCount(props) {
     }
 
     return(
+        <>
         <div id='contador'>
             <button onClick={subtract} className="contadorButtons" >-</button>
             <h4>{cantidad}</h4>
             <button onClick={ add } className="contadorButtons" >+</button>
         </div>
+        <button 
+            id="infoItemButton" 
+            onClick={()=>{
+                onAddToCart(cantidad)
+            }}
+        >Añadir al carrito</button>
+        </>
     )
 }
 
