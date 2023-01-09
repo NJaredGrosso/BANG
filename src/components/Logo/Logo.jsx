1992de8935625e650218793a48d4ed65b4ba6213
NJaredGrosso <jaredgrosso2001@gmail.com>
2023-01-09 14:33:34 -0300

    Solucion de error

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,14 @@
-import "./logo.css"
-import { Link } from "react-router-dom"
+import "./logo.css";
+import { Link } from "react-router-dom";
 
 function Logo() {
-  return (
-    <div className="logo">
-        <Link to="/"><img src={require("../../assets/logo.png")} /></Link>
-    </div>
-  )
+	return (
+		<div className="logo">
+			<Link to="/BANG">
+				<img src={require("../../assets/logo.png")} />
+			</Link>
+		</div>
+	);
 }
 
-export default Logo
-
+export default Logo;
